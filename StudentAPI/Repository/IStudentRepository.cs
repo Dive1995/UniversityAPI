cca860a646e6499c79dc4900743fbeed5ec36895
Diveshan <diveshan1995@gmail.com>
2022-12-10 08:22:49 +0530

    added student repository

---
+++ StudentAPI/Repository/IStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using StudentAPI.Models;
+
+namespace StudentAPI.Repository
+{
+	public interface IStudentRepository
+	{
+		Student GetStudent(string registrationId);
+		ICollection<Student> GetAllStudents();
+		ICollection<Student> GetStudentsByDegree(int degreeId);
+		void AddNewStudent(Student student);
+		Student UpdateStudent(Student student);
+		void SaveChanges();
+	}
+}
+
---
+++ StudentAPI/Repository/StudentRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using StudentAPI.Models;
+
+namespace StudentAPI.Repository
+{
+	public class StudentRepository : IStudentRepository
+	{
+        private readonly UniversityContext _context;
+
+        public StudentRepository(UniversityContext context)
+		{
+            _context = context;
+		}
+
+        public void AddNewStudent(Student student)
+        {
+            _context.Students.Add(student);
+
+        }
+
+        public ICollection<Student> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Student> GetStudentsByDegree(int degreeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student UpdateStudent(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
+
